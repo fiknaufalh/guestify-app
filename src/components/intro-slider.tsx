cc6654023ee01c3b968837b551c8a45d69569c00
fiknaufalh <18221096@mahasiswa.itb.ac.id>
2024-04-24 23:00:17 +0700

    fix: integrate nativewind with expo router

---
+++ src/components/intro-slider.tsx
@@ -0,0 +1,76 @@
+import AppIntroSlider from "react-native-app-intro-slider";
+import { View, Text, Image } from "react-native";
+import { COLORS, SIZES } from "../constants/theme";
+import React from "react";
+
+const slides = [
+  {
+    id: 1,
+    title: "Solusi Manajemen Acaramu!",
+    description:
+      "Jelajahi kecanggihan teknologi manajemen tamu yang inovatif dan nikmati pengalaman acara yang mulus bersama Guestify",
+    image: require("../assets/onboard-1.png"),
+  },
+  {
+    id: 2,
+    title: "Solusi Praktis!",
+    description:
+      "Dengan Guestify, nikmati kemudahan mengelola RSVP dan Digital Guest Book. Jadikan setiap acara penuh kenangan!",
+    image: require("../assets/onboard-2.png"),
+  },
+  {
+    id: 3,
+    title: "Solusi Ekonomis!",
+    description:
+      "Berkumpul, merayakan, dan mengelola acara dengan mudah. Guestify hadir untuk memastikan setiap detik acaramu berjalan dengan sempurna",
+    image: require("../assets/onboard-3.png"),
+  },
+];
+
+type IntroSlideProps = {
+  setShowHomePage: (value: boolean) => void;
+};
+
+export default function IntroSlide({ setShowHomePage }: IntroSlideProps) {
+  const buttonLabel = (label: any) => {
+    return (
+      <View className="p-3">
+        <Text className="text-lg font-semibold text-purple-dark">{label}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <AppIntroSlider
+      data={slides}
+      renderItem={({ item }) => {
+        return (
+          <View className="flex-1 items-center p-8 pt-20">
+            <Image
+              source={item.image}
+              className={`w-[${SIZES.width - 80}] h-[400]`}
+              resizeMode="contain"
+            />
+            <Text className="text-2xl text-purple-dark font-bold">
+              {item.title}
+            </Text>
+            <Text className="text-m text-purple-dark text-center pt-2">
+              {item.description}
+            </Text>
+          </View>
+        );
+      }}
+      activeDotStyle={{
+        backgroundColor: COLORS.purple,
+        width: 30,
+      }}
+      showSkipButton
+      renderNextButton={() => buttonLabel("Next")}
+      renderSkipButton={() => buttonLabel("Skip")}
+      renderDoneButton={() => buttonLabel("Done")}
+      onDone={() => {
+        setShowHomePage(true);
+      }}
+    />
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,26 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        fontFamily: {
-            sans: ["Helvetica", "Arial", "sans-serif"]
-        },
-        colors: {
-            "purple-dark": "#31013F",
-            purple: "#690895",
-            "purple-light": "#BA7DB0",
-            yellow: "#F9D264",
-            orange: "#E9A400",
-            brown: "#601C0E",
-            white: "#FFFFFF",
-            black: "#000000"
-        },
-        extend: {}
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Helvetica", "Arial", "sans-serif"],
+      },
+      colors: {
+        "purple-dark": "#31013F",
+        "purple-light": "#BA7DB0",
+        purple: "#690895",
+        yellow: "#F9D264",
+        orange: "#E9A400",
+        brown: "#601C0E",
+        white: "#FFFFFF",
+        black: "#000000",
+      },
     },
-    plugins: []
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [],
 };
