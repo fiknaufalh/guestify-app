a8bc1aa577471a96d9580d9ed702aec9c9c240ef
oncarrozaqy <oncarrozaqyy@gmail.com>
2024-05-30 21:38:41 +0700

    fix: edit dikit dashboard dan tabel

--- src/screens/dashboard/tabrsvp.tsx
+++ src/screens/dashboard/tabrsvp.tsx
@@ -47,9 +47,9 @@ function RSVPDashboard({ navigation, route }: RSVPDashboardProps) {
                 return;
             }
 
-            const confirmed = rsvps.filter(rsvp => rsvp.status === 'confirmed').length;
-            const pending = rsvps.filter(rsvp => rsvp.status === 'pending').length;
-            const declined = rsvps.filter(rsvp => rsvp.status === 'declined').length;
+            const confirmed = rsvps.filter(rsvp => rsvp.status === 'Confirmed').length;
+            const pending = rsvps.filter(rsvp => rsvp.status === 'Pending').length;
+            const declined = rsvps.filter(rsvp => rsvp.status === 'Declined').length;
 
             setTotalConfirmed(confirmed);
             setTotalPending(pending);
@@ -68,7 +68,7 @@ function RSVPDashboard({ navigation, route }: RSVPDashboardProps) {
             <View className="bg-white flex-1 h-full">
                 <TotalConfirmed label="Total Confirmed" total={totalConfirmed} description="undangan" />
                 <View className='justify-center items-center'>
-                    <DashboardCard label={"Confirmed"} icon="supervised-user-circle" status={totalConfirmed} invitees={totalGuests} pax={`${totalConfirmed / totalGuests} %`} color={"#3EC04B"} />
+                    <DashboardCard label={"Confirmed"} icon="supervised-user-circle" status={totalConfirmed} invitees={totalGuests} pax={`${((totalConfirmed / totalGuests) * 100).toFixed(2)} %`} color={"#3EC04B"} />
                     <DashboardCard label={"Pending"} icon="pending-actions" status={totalPending} invitees={totalGuests} pax={`${((totalPending / totalGuests) * 100).toFixed(2)} %`} color={"#E9A400"} />
                     <DashboardCard label={"Declined"} icon="cancel" status={totalDeclined} invitees={totalGuests} pax={`${((totalDeclined / totalGuests) * 100).toFixed(2)} %`} color={"#ED4C4D"} />
                 </View>
